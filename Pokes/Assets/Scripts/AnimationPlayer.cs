fd14b617b2a292a8d3e6885b1ebe548a46051bed
festaumberto182 <festaumberto@gmail.com>
2020-01-20 12:43:40 +0100

    pokes

---
+++ Pokes/Assets/Scripts/AnimationPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    [SerializeField] PlayableDirector director;
+
+    private void Start() {
+        director.played += DisableInput;
+        director.stopped += EnableInput;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        director.Play();
+    }
+
+    void DisableInput(PlayableDirector p){
+        GameObject player  = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<ThirdPersonCharacter>().Move(this.transform.position, false,false);
+        player.GetComponent<ThirdPersonUserControl>().enabled = false;
+
+    }
+    void EnableInput(PlayableDirector p){
+        GameObject player  = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<ThirdPersonUserControl>().enabled = true;
+    }
+}

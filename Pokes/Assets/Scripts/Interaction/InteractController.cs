ba3e7a749f345bd4988b646ada5248ac9b82661c
festaumberto182 <festaumberto@gmail.com>
2020-01-20 14:15:51 +0100

    test player finding interactable objects

---
+++ Pokes/Assets/Scripts/Interaction/InteractController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pokes.Interactable {
+    public class InteractController : MonoBehaviour
+    {
+        [SerializeField] float rayLength = 4f;
+        private RaycastHit targetHit;
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayLength, Color.red,0.5f);
+
+            if(Physics.Raycast(transform.position, transform.forward, out targetHit, rayLength)){
+                print("something in range");
+                if(targetHit.collider.GetComponent<Interactable>() != null){
+                    print("interactable in range");
+                    targetHit.collider.GetComponent<Interactable>().Interact();
+                }
+            }
+        }
+    }
+}
---
+++ Pokes/Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pokes.Interactable {
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField] GameObject hintObject;
+        // Start is called before the first frame update
+        void Start()
+        {
+            hintObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void Interact(){
+            Vector3 hintPosition = transform.position;
+            hintPosition.y+=1f;
+
+            hintObject.SetActive(true);
+            hintObject.transform.position = hintPosition;
+        }
+    }
+}
